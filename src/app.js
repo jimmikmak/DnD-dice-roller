b94b66567e9a63c8bdc696491dc066e3f45fef90
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2020-12-09 12:23:02 +1100

    jQuery update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,34 @@
 console.log("App is running");
 
-$("#dice-roller").on("click", () => {
-  return Math.floor(Math.random() * 4);
-});
+const randomRollOnClick = (event) => {
+  event.preventDefault();
+  console.log("form Roll");
+
+  // Random dice rolls on click
+  $("#dice-roller-d4").on("click", () => {
+    console.log(Math.round(Math.random() * 4) + 1);
+  });
+  $("#dice-roller-d6").on("click", () => {
+    console.log(Math.round(Math.random() * 6) + 1);
+  });
+  $("#dice-roller-d8").on("click", () => {
+    console.log(Math.round(Math.random() * 8) + 1);
+  });
+  $("#dice-roller-d10").on("click", () => {
+    console.log(Math.round(Math.random() * 10) + 1);
+  });
+  $("#dice-roller-d12").on("click", () => {
+    console.log(Math.round(Math.random() * 12) + 1);
+  });
+  $("#dice-roller-d20").on("click", () => {
+    console.log(Math.round(Math.random() * 20) + 1);
+  });
+};
+
+$("#dice-roller-d4").on("Roll", randomRollOnClick);
+$("#dice-roller-d4").click();
+$("#dice-roller-d6").click();
+$("#dice-roller-d8").click();
+$("#dice-roller-d10").click();
+$("#dice-roller-d12").click();
+$("#dice-roller-d20").click();
