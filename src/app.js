739fd43c37542e6f3b34fcf7f2212f9cd7d10e88
jimmikmak <73326512+jimmikmak@users.noreply.github.com>
2020-12-12 16:50:55 +1100

    Multiple dice results

--- src/app.js
+++ src/app.js
@@ -4,38 +4,69 @@ console.log("App is running");
 
 // Look at this d4 die, when clicked, do something
 $("#dice-roller-d4").on("click", () => {
-  // Do a Math.random, store as a variable
-  const result = Math.floor(Math.random() * 4) + 1;
-  // If more than one die is to rolled,
-  // roll, plus calculate total of random results
-const numOfDice = diceArray [];
-for (let i = 0; < diceArray.length; i++) {
-  return (result[i]);
-}
-
+  let totalDiceRollSum = 0;
+  // integer value of the number of rolls selected
+  const numOfRolls = parseInt($("#count_1").val());
+  console.log(numOfRolls);
+  // If more than one die is rolled,
+  // roll, and calculate the total of the random results
+  for (let i = 0; i < numOfRolls; i++) {
+    let result = Math.floor(Math.random() * 4) + 1;
+    totalDiceRollSum = totalDiceRollSum + result;
+  }
   // Look for a div labeled: "d4-generated-num" and change the HTML
   // to be the resulting integer
-  document.getElementById("d4-generated-number").innerHTML = result;
+  document.getElementById("d4-generated-number").innerHTML = totalDiceRollSum;
 });
 $("#dice-roller-d6").on("click", () => {
-  const result = Math.floor(Math.random() * 6) + 1;
-  document.getElementById("d6-generated-number").innerHTML = result;
+  let totalDiceRollSum = 0;
+  const numOfRolls = parseInt($("#count_2").val());
+  console.log(numOfRolls);
+  for (let i = 0; i < numOfRolls; i++) {
+    let result = Math.floor(Math.random() * 6) + 1;
+    totalDiceRollSum = totalDiceRollSum + result;
+  }
+  document.getElementById("d6-generated-number").innerHTML = totalDiceRollSum;
 });
 $("#dice-roller-d8").on("click", () => {
-  const result = Math.floor(Math.random() * 8) + 1;
-  document.getElementById("d8-generated-number").innerHTML = result;
+  let totalDiceRollSum = 0;
+  const numOfRolls = parseInt($("#count_3").val());
+  console.log(numOfRolls);
+  for (let i = 0; i < numOfRolls; i++) {
+    let result = Math.floor(Math.random() * 8) + 1;
+    totalDiceRollSum = totalDiceRollSum + result;
+  }
+  document.getElementById("d8-generated-number").innerHTML = totalDiceRollSum;
 });
 $("#dice-roller-d10").on("click", () => {
-  const result = Math.floor(Math.random() * 10) + 1;
-  document.getElementById("d10-generated-number").innerHTML = result;
+  let totalDiceRollSum = 0;
+  const numOfRolls = parseInt($("#count_4").val());
+  console.log(numOfRolls);
+  for (let i = 0; i < numOfRolls; i++) {
+    let result = Math.floor(Math.random() * 10) + 1;
+    totalDiceRollSum = totalDiceRollSum + result;
+  }
+  document.getElementById("d10-generated-number").innerHTML = totalDiceRollSum;
 });
 $("#dice-roller-d12").on("click", () => {
-  const result = Math.floor(Math.random() * 12) + 1;
-  document.getElementById("d12-generated-number").innerHTML = result;
+  let totalDiceRollSum = 0;
+  const numOfRolls = parseInt($("#count_5").val());
+  console.log(numOfRolls);
+  for (let i = 0; i < numOfRolls; i++) {
+    let result = Math.floor(Math.random() * 12) + 1;
+    totalDiceRollSum = totalDiceRollSum + result;
+  }
+  document.getElementById("d12-generated-number").innerHTML = totalDiceRollSum;
 });
 $("#dice-roller-d20").on("click", () => {
-  const result = Math.floor(Math.random() * 20) + 1;
-  document.getElementById("d20-generated-number").innerHTML = result;
+  let totalDiceRollSum = 0;
+  const numOfRolls = parseInt($("#count_6").val());
+  console.log(numOfRolls);
+  for (let i = 0; i < numOfRolls; i++) {
+    let result = Math.floor(Math.random() * 20) + 1;
+    totalDiceRollSum = totalDiceRollSum + result;
+  }
+  document.getElementById("d20-generated-number").innerHTML = totalDiceRollSum;
 });
 
 // Plus counter inputs
